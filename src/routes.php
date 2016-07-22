a633d804a490e9fab81c02c304c2b6fe41cfa2e4
unknown <perseus.magbutay@gmail.com>
2016-07-23 03:26:41 +0800

    updated index, added public path var

--- src/routes.php
+++ src/routes.php
@@ -5,6 +5,10 @@ $app->get('[/]', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
+    $uri = $request->getUri();
+
+    $data = ['publicPath' => $uri->getBasePath() ];
+
     // Render index view
-    return $this->view->render($response, 'index.html', $args);
+    return $this->view->render($response, 'index.html', $data);
 });
