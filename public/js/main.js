1b3609692c9931e6ce2676f4b1824452e6329949
jesperseurin <jesperseunri@gmail.com>
2016-07-28 01:27:27 +0800

    routing

--- public/js/main.js
+++ public/js/main.js
@@ -5,8 +5,14 @@ ngSlim.config(function($stateProvider, $urlRouterProvider, $locationProvider){
   
   // For any unmatched url, send to /route1
   //$urlRouterProvider.otherwise("/route1")
-  
+  $urlRouterProvider.otherwise("/404");
+
   $stateProvider
+    .state('home', {
+        url: "/",
+        templateUrl: "pages/home.html"
+    })
+
     .state('about', {
         url: "/about",
         templateUrl: "pages/about.html"
@@ -15,6 +21,11 @@ ngSlim.config(function($stateProvider, $urlRouterProvider, $locationProvider){
     .state('contact', {
         url: "/contact",
         templateUrl: "pages/contact.html"
+    })
+      
+    .state('notfound', {
+        url: "/404",
+        templateUrl: "pages/404.html"
     })
 
   // use the HTML5 History API
