062f3c52b72e5ed67fc43351801d2b3226a762c4
unknown <perseus.magbutay@gmail.com>
2016-08-07 19:08:57 +0800

    added Handlers and Models

--- src/routes.php
+++ src/routes.php
@@ -13,8 +13,17 @@ $app->get('[/]', function ($request, $response, $args) {
     return $this->view->render($response, 'index.php', $data);
 });
 
-$app->get('/api/testendpoint', function ($request, $response, $args) {
-	$data = array('name' => 'Bob', 'age' => 40);
-	$newResponse = $response->withJson($data, 201);
-	return $response;
+
+
+$app->group('/api', function () {
+	$this->get('/testendpoint', function ($request, $response, $args) {
+		$data = array('name' => 'Bob', 'age' => 40);
+		$newResponse = $response->withJson($data, 201);
+		return $response;
+	});
+	$this->get('/users', function ($request, $response, $args) {
+		$data = array('module' => 'Users', 'success' => true, 'msg' => 'Users Test');
+		$newResponse = $response->withJson($data, 201);
+		return $response;
+	});
 });
